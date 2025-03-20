36b262eb6dd4c162158d188a560aa6a095a4f950
bornsoon <hyolim922@hotmail.com>
2025-03-20 23:18:18 +0900

    공감버튼 service 클래스

--- SKL_spring/src/main/java/app/labs/emoji/service/EmojiService.java
+++ SKL_spring/src/main/java/app/labs/emoji/service/EmojiService.java
@@ -2,7 +2,6 @@ package app.labs.emoji.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public interface EmojiService {
     List<Map<String, Object>> getEmojiCount(int BoardId);
